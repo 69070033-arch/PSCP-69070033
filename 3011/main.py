2dfedb5468897531eddaf6511c05dff0b7391945
69070033-arch <69070033@kmitl.ac.th>
2026-07-06 19:56:40 +0700

    Update main.py

--- 3011/main.py
+++ 3011/main.py
@@ -2,4 +2,11 @@
 x = input()
 y = input()
 
-print("Orange") if x == "Red" and y == "Yellow" else print("Error")
+if (x == "Red" or x == "Yellow") and (y == "Yellow" or y == "Red"):
+    print("Orange")
+elif (x == "Red" or x == "Blue") and (y == "Blue" or y == "Red"):
+    print("Violet")
+elif (x == "Blue" or x == "Yellow") and (y == "Yellow" or y == "Blue"):
+    print("Green")
+else:
+    print("Error")
