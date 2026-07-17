0a48521b516190512a6c9714dc63842bf04b50dc
69070033-arch <69070033@kmitl.ac.th>
2026-07-17 14:36:06 +0700

    wad

--- 3002/main.py
+++ 3002/main.py
@@ -4,3 +4,7 @@ name = input()
 surname =input()
 age = int(input())
 
+if len(name) >= 5 and len(surname) >= 5:
+    print(f"{name[:2]}{surname[-1]}{age%10}")
+else:
+    print(f"{name[0]}{age}{surname[-1]}")
